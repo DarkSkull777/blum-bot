0debd89c0c6cf758d016066bffe4fd62ea565aa8
Dimas a.k.a Dymles <59839225+DarkSkull777@users.noreply.github.com>
2024-06-21 20:59:04 +0700

    fix problem

--- blumbot.py
+++ blumbot.py
@@ -39,7 +39,7 @@ print(colored("  [ ", 'white') + colored("2 ", 'light_green') + colored("] ", 'w
 
 while True:
     try:
-        language_choice = int(input("\n  {putih}blum.bot@main:~# {reset}"))
+        language_choice = int(input(f"\n  {putih}blum.bot@main:~# {reset}"))
         if language_choice in [1, 2]:
             break
         else:
