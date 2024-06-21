51b4db1dd37f4fcd948638f2a1baf4651b1757a8
Dimas a.k.a Dymles <59839225+DarkSkull777@users.noreply.github.com>
2024-06-21 21:06:49 +0700

    Update blumbot.py

--- blumbot.py
+++ blumbot.py
@@ -76,8 +76,8 @@ if window_name == '2':
 check = gw.getWindowsWithTitle(window_name)
 if not check:
     print(window_not_found.format(window_name))
-    print(f" {putihmerah}EN:{reset}\n{putihmerah}   Make sure you use the TelegramDesktop application (not Telegram Web).\n   And have opened the Blum bot on your TelegramDesktop.\n   Until the Blum window is available{reset}")
-    print(f" {putihmerah}ID:{reset}\n{putihmerah}   Pastikan kamu menggunakan aplikasi TelegramDesktop (bukan Telegram Web).\n   Dan telah membuka Blum bot di TelegramDesktop kamu.\n   Hingga tersedia window Blum nya{reset}")
+    print(f" {putihmerah}EN:{reset}\n   {putihmerah}Make sure you use the TelegramDesktop application (not Telegram Web).{reset}\n   {putihmerah}And have opened the Blum bot on your TelegramDesktop.{reset}\n   {putihmerah}Until the Blum window is available{reset}")
+    print(f" {putihmerah}ID:{reset}\n   {putihmerah}Pastikan kamu menggunakan aplikasi TelegramDesktop (bukan Telegram Web).{reset}\n   {putihmerah}Dan telah membuka Blum bot di TelegramDesktop kamu.{reset}\n   {putihmerah}Hingga tersedia window Blum nya{reset}")
 else:
     print(window_found.format(window_name))
     telegram_window = check[0]
